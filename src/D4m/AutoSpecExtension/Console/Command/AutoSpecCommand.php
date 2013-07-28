e369fdf251958f5732a0fab2e7e6868d65147e8e
raul782 <raulrodriguez782@gmail.com>
2013-07-27 23:52:35 -0500

    Added phpspec run in AutoSpecCommand

--- src/D4m/AutoSpecExtension/Console/Command/AutoSpecCommand.php
+++ src/D4m/AutoSpecExtension/Console/Command/AutoSpecCommand.php
@@ -41,15 +41,16 @@ class AutoSpecCommand extends Command
         $watcher->track('src', $src);
         $watcher->track('spec', $spec);
 
-        $watcher->addListener('src', function (FilesystemEvent $event) {
-            //echo shell_exec('bin/phpspec');
-            echo $event->getResource() . 'was' . $event->getTypeString();
+        $watcher->addListener('src', function (FilesystemEvent $event) use ($output) {
+            $output->writeln("PHPSpec AutoRunning ....");
+            $output->writeln(shell_exec('bin/phpspec'));
         });
-        /*
-        $watcher->addListener('spec', function (FilesystemEvent $event) {
-            echo shell_exec('bin/phpspec');
+
+        $watcher->addListener('spec', function (FilesystemEvent $event) use ($output) {
+            $output->writeln("PHPSpec AutoRunning ....");
+            $output->writeln(shell_exec('bin/phpspec'));
         });
-        */
+
         $watcher->start();
 
     }
